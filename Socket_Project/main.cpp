0f74b07d69ba3705d8cfd3be4c12a5a47f730434
Fabien <m4gyars@MacBook-Pro-de-Fabien.local>
2014-05-07 12:00:41 -0700

    Initial Commit

---
+++ Socket_Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Socket_Project
+//
+//  Created by Fabien on 07/05/2014.
+//  Copyright (c) 2014 ___EFREI-GENET-BOURGEAUX-LE_VAN___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
